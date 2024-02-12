a63527b70d2be8272b97e2afe8a9b573874ba4f3
Akilesh <ja694@uowmail.edu.au>
2024-02-12 19:14:15 +0800

    Fix theme switch visibility and update tailwindcss version

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
-import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import ThemeContextProvider from "@/context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
