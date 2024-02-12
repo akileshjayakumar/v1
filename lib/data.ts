7967f8b0f9abf047389ded3a9b3b6950389c1ea2
Akilesh <ja694@uowmail.edu.au>
2024-02-12 19:51:49 +0800

    Update project images and descriptions

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,10 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import gptclone from "@/public/gpt-clone.png";
+import flipcoin from "@/public/flip-coin.png";
+import passwordgen from "@/public/password-gen.png";
+import cpflogo from "@/public/cpf-logo.png";
 
 export const links = [
   {
@@ -31,52 +32,36 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Automation Intern | CPF Board",
+    location: "Singapore",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "Built HR automation bot using GPT-3.5 API, Power Automate, & Python. Reduced user testing time by 30% with automated test cases in Python. Improved software quality with automated tests using Pair & prompt engineering. Gained expertise in GovTech tools: SGTS, Pair, LaunchPad, & GCC.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "Dec 2023 - Present",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "ChatGPT Clone Project",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Developed a ChatGPT clone with frontend (HTML, CSS, JavaScript) and backend (Node.js, Express) components, leveraging GPT-3.5 API for AI interactions in real-time.",
+    tags: ["HTML", "CSS", "JavaScript", "Node.js", "Express", "OpenAI API"],
+    imageUrl: gptclone,
   },
   {
-    title: "rmtDev",
+    title: "Flip A Coin Game",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      "Interactive  Coin Flip Game simulating coin tosses with clickable fun. Developed with HTML, CSS, and JavaScript for engaging gameplay.",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: flipcoin,
   },
   {
-    title: "Word Analytics",
+    title: "Password Generator",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Password Generator crafting robust and unique passwords. Implemented using HTML, CSS, and JavaScript for a user-friendly experience.",
+    tags: ["HTML", "CSS", "JavaScript"],
+    imageUrl: passwordgen,
   },
 ] as const;
 
