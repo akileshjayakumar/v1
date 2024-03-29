a8099f18678a7c4c3e6f6720f105b4100764f5d5
Akilesh <ja694@uowmail.edu.au>
2024-03-29 14:34:24 +0800

    Update date

--- components/experience.tsx
+++ components/experience.tsx
@@ -35,7 +35,7 @@ export default function Experience() {
               borderRight: "8px solid #fff",
             }}
             date={item.date}
-            dateClassName="text-lg font-semibold"
+            dateClassName="text-lg m-[1rem] font-semibold text-white"
             icon={<item.icon />}
             iconStyle={{
               background: theme === "light" ? "#60a5fa" : "#000000",
