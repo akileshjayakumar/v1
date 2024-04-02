453d73c63a8129ecc57f0754254c963bc388df90
Akilesh <ja694@uowmail.edu.au>
2024-04-02 09:22:16 +0800

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 tracking-wider text-4xl text-center text-white">
-      <h3 className="mb-[4rem]">
+    <footer className="mb-10 px-4 tracking-wider text-center text-white">
+      <h3 className="mb-[7rem]">
         {/* Updated styling and animation */}
         <a
           href="https://github.com/akileshjayakumar?tab=repositories"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block py-2 px-4 tracking-wider bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white font-bold rounded-full shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:rotate-2"
+          className="inline-block text-xl py-6 px-6 
+          tracking-wider bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 text-white 
+          font-bold rounded-full shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+          // Removed hover:rotate-2
         >
           more projects on my github!
         </a>
