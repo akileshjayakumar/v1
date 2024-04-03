01df936fac81ad55c9e97740c085ffdc1aeac784
Akilesh <ja694@uowmail.edu.au>
2024-04-03 13:34:04 +0800

    Update styles in Education component

--- components/education.tsx
+++ components/education.tsx
@@ -20,11 +20,11 @@ export default function Education() {
     <section id="education" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>EDUCATION</SectionHeading>
       <div className="space-y-6">
-        <div className="bg-white shadow-md rounded-lg p-4">
-          <h3 className="text-lg font-semibold">test</h3>
-          <h4 className="text-md">test</h4>
-          <p className="text-sm">test</p>
-          <ul className="list-disc list-inside">
+        <div className="bg-black shadow-md rounded-lg p-4">
+          <h3 className="text-lg text-white font-semibold">test</h3>
+          <h4 className="text-md text-white">test</h4>
+          <p className="text-sm text-white">test</p>
+          <ul className="list-disc list-inside text-white">
             <li>1</li>
           </ul>
           <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
